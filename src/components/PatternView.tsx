6b70c588a2843876b747ab0ca903195ce6bdfc8d
Kangal Dev <woof@kangaltoken.com>
2021-07-15 18:38:29 +0300

    Update pattern

--- src/components/PatternView.tsx
+++ src/components/PatternView.tsx
@@ -9,7 +9,7 @@ export default function PatternView(props: IPatternView) {
   return (
     <div className="z-0 top-0 left-0 h-full w-14 opacity-30 absolute overflow-hidden">
       <div>
-        <div className="absolute-center" style={{ left: 0, top: 20 }}>
+        <div className="absolute-center" style={{ left: 11, top: 22 }}>
           {Array.from({ length: rows }, (_, rowIndex) => (
             <div key={rowIndex} className="flex">
               {Array.from({ length: columns }, (_, columnIndex) => (
