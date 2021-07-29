87172f30c768d68304ad61a5b79ec3f8451bae0e
Kangal Dev <kangaltoken@gmail.com>
2021-07-29 20:02:13 +0300

    Minor styling

--- src/pages/Bridge.tsx
+++ src/pages/Bridge.tsx
@@ -1,3 +1,7 @@
 export default function Bridge() {
-  return <div className="text-center">Bridge</div>;
+  return (
+    <div className="container mx-auto px-4 mt-4">
+      <div className="text-center">Bridge</div>
+    </div>
+  );
 }
